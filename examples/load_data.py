2bc279175ebc691713ad15d6d575abc3eb0ec2ac
Dylan Ray <ddray@email.unc.edu>
2021-07-30 15:38:01 -0400

    stuff

---
+++ examples/load_data.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import matplotlib.pyplot as plt
+
+import os
+import argparse
+import logging
+
+import random
+
+################ DESCRIPTION:
+### Loads a random X variable from the downloaded and unzipped folder and plots it
+
+def parse_args():
+    # Argument Parser
+    parser = argparse.ArgumentParser()
+    # my args
+    parser.add_argument("--verbose", action = "store_true", help = "display messages")
+    parser.add_argument("--idir", default = "data/ECGDataDenoised", help = "path to directory of CSV files (X) of 12-lead ECG signals")
+
+    args = parser.parse_args()
+
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+        logging.debug("running in verbose mode")
+    else:
+        logging.basicConfig(level=logging.INFO)
+
+    return args
+
+def main():
+    args = parse_args()
+    
+    # os.listdir gets a list of files from args.idir
+    # for each of those we join the path the of input directory for the full path
+    ifiles = [os.path.join(args.idir, u) for u in os.listdir(args.idir)]
+    
+    # shuffle the list and pick the first one
+    random.shuffle(ifiles)
+    ifile = ifiles[0]
+    
+    # Load the data using NumPy
+    # we specify that the numbers are comma separated
+    X = np.loadtxt(ifile, delimiter = ',')
+    logging.info('X has shape {}...'.format(X.shape))
+    
+    fig, axes = plt.subplots(ncols = 2)
+    
+    # plot the 2d array as an image with an aspect ratio = 1
+    axes[0].imshow(X, extent = (0, 1, 0, 1))
+    
+    axes[1].plot(X[:,0])
+    axes[1].set_xlabel('time')
+    axes[1].set_ylabel('voltage')
+    
+    plt.show()
+
+if __name__ == '__main__':
+    main()
---
+++ examples/training_template.py
@@ -0,0 +1,167 @@
+# -*- coding: utf-8 -*-
+import os
+import logging
+import argparse
+
+from torch.nn import CrossEntropyLoss, KLDivLoss, NLLLoss, SmoothL1Loss, BCELoss, BCEWithLogitsLoss
+import torch
+
+from torch import nn
+
+import torch.optim as optim
+
+def parse_args():
+    # Argument Parser
+    parser = argparse.ArgumentParser()
+    # my args
+    parser.add_argument("--verbose", action = "store_true", help = "display messages")
+    parser.add_argument("--weights", default = "None")
+    
+    parser.add_argument("--n_epochs", default = "100", help = "number of times to go through the training set")
+    parser.add_argument("--lr", default = "0.001", help = "learning rate for Adam optimizer")
+    
+    parser.add_argument("--odir", default = "training_output")
+    parser.add_argument("--tag", default = "test")
+    args = parser.parse_args()
+
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+        logging.debug("running in verbose mode")
+    else:
+        logging.basicConfig(level=logging.INFO)
+
+    if args.odir != "None":
+        if not os.path.exists(args.odir):
+            os.system('mkdir -p {}'.format(args.odir))
+            logging.debug('root: made output directory {0}'.format(args.odir))
+    # ${odir_del_block}
+
+    return args
+
+def main():
+    args = parse_args()
+
+    # do we have a GPU?
+    if torch.cuda.is_available():
+        device = torch.device('cuda')
+    else:
+        device = torch.device('cpu')
+
+    ### instantiate your model here ###
+    # model = MyModel()
+    model = model.to(device)
+    
+    # do we have weights?
+    if args.weights != "None":
+        checkpoint = torch.load(args.weights, map_location = device)
+        model.load_state_dict(checkpoint)
+
+    ### Define your data generator class here
+    ## generator = MyGenerator(*args)
+    ## val_generator = MyValGenerator(*args)
+
+    ### Define your criterion (loss) here
+    ## criterion = nn.NLLLoss() # negative log-likelihood seems like a good choice
+    optimizer = optim.Adam(model.parameters(), lr = 0.001)
+
+    min_val_loss = np.inf
+    # count to stop early if validation loss isn't going down
+    early_count = 0
+
+    # save some metrics to look at later...
+    history = dict()
+    history['epoch'] = []
+    history['loss'] = []
+    history['val_loss'] = []
+    # ${define_extra_history}
+
+    print('training...')
+    for ix in range(int(args.n_epochs)):
+        model.train()
+
+        losses = []
+        accuracies = []
+
+        for ij in range(len(generator)):
+            optimizer.zero_grad()
+            x, y = generator.get_batch()
+
+            x = x.to(device)
+            y = y.to(device)
+
+            y_pred = model(x)
+
+            loss = criterion(y_pred, y)
+            loss.backward()
+            optimizer.step()
+            losses.append(loss.item())
+
+            # compute accuracy in CPU with sklearn
+            y_pred = np.exp(y_pred.detach().cpu().numpy())
+            y = y.detach().cpu().numpy()
+
+            y_pred = np.argmax(y_pred, axis=1)
+
+            # append metrics for this epoch
+            accuracies.append(accuracy_score(y, y_pred))
+
+            # print progress to console
+            # loss should be going down...
+            if (ij + 1) % 10 == 0:
+                logging.info(
+                    'root: Epoch {0}, step {3}: got loss of {1}, acc: {2}'.format(ix, np.mean(losses),
+                                                                                  np.mean(accuracies), ij + 1))
+        ## VERY IMPORTANT if your model has stochastic regularization like Dropout
+        ## or batch normalization, etc.
+        model.eval()
+
+        val_losses = []
+        val_accs = []
+        for step in range(len(val_generator)):
+            with torch.no_grad():
+                x, y = val_generator.get_batch()
+
+                x = x.to(device)
+                y = y.to(device)
+    
+                loss = criterion(y_pred, y)
+                # compute accuracy in CPU with sklearn
+                y_pred = np.exp(y_pred.detach().cpu().numpy())
+                y = y.detach().cpu().numpy()
+
+                y_pred = np.argmax(y_pred, axis=1)
+
+                # append metrics for this epoch
+                val_accs.append(accuracy_score(y, y_pred))
+                val_losses.append(loss.detach().item())
+
+        val_loss = np.mean(val_losses)
+
+        logging.info(
+            'root: Epoch {0}, got val loss of {1}, acc: {2} '.format(ix, val_loss, np.mean(val_accs)))
+
+        history['epoch'].append(ix)
+        history['loss'].append(np.mean(losses))
+        history['val_loss'].append(np.mean(val_losses))
+        # ${save_extra_history}
+
+        val_loss = np.mean(val_losses)
+        if val_loss < min_val_loss:
+            min_val_loss = val_loss
+            print('saving weights...')
+            torch.save(model.state_dict(), os.path.join(args.odir, '{0}.weights'.format(args.tag)))
+
+            early_count = 0
+        else:
+            early_count += 1
+
+            # early stop criteria
+            if early_count > int(args.n_early):
+                break
+        
+        ## used to shuffle the data and let the generator know were iterating through it again
+        generator.on_epoch_end()
+
+        # save the training history using Pandas (CSV library)
+        df = pd.DataFrame(history)
+        df.to_csv(os.path.join(args.odir, '{}_history.csv'.format(args.tag)), index = False)
