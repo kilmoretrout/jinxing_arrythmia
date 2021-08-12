113a2e96ef7a32fe17712624740d140f35ea25f0
xstblu <65331874+xstblu@users.noreply.github.com>
2021-08-12 06:05:15 -0700

    Update training_template.py

--- examples/training_template.py
+++ examples/training_template.py
@@ -11,6 +11,9 @@ from torch import nn
 import torch.optim as optim
 import pandas as pd
 
+import torch.nn as nn
+import torch.nn.functional as F
+
 def parse_args():
     # Argument Parser
     parser = argparse.ArgumentParser()
@@ -49,6 +52,30 @@ def main():
         device = torch.device('cpu')
 
     ### instantiate your model here ###
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1d = nn.Conv1d(16, 33, 3)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+net = Net()
+
+
+net = Net()
     # model = MyModel()
     model = model.to(device)
     
